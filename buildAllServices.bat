db0fb732df479e7ea722c2583b11e9ad6d458bb3
shivaramr <85446156+shivaramr@users.noreply.github.com>
2021-09-22 16:14:58 +0530

    dotnet files inside

---
+++ buildAllServices.bat
@@ -0,0 +1,6 @@
+
+
+dotnet clean <Location of respective project file>
+dotnet build -p:Version=3.1 <Location of respective project file>
+
+pause
---
+++ dotnetcoreRunServices.bat
@@ -0,0 +1,5 @@
+set WORKING_ENV=Development
+
+start cmd /k dotnet <Location of dll file inside generated Build folder>
+
+cmd /k
